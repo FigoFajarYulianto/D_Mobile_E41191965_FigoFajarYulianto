e94ea12796f3d047913f226fff08d07ffefb98c4
FigoFajarYulianto <figofajar07@gmail.com>
2021-06-25 16:25:41 +0700

    db movie

---
+++ JsonMovieDB/app/src/main/java/com/themoviedb/myapplication/ApiEndpoint.java
@@ -0,0 +1,13 @@
+package com.themoviedb.myapplication;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=8a81511a8af946865d98a7fd36ea6b15";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
